0b67a2d102217b15553953b913a5ddb6500675c3
Sara Coelho Costa <costa.saracoelho@gmail.com>
2023-08-11 17:42:54 -0300

    Delete Programação Java/CampoMinadoSwing directory

--- Programação Java/CampoMinadoSwing/src/br/com/cod3r/campoMinado/modelo/CampoEvento.java
+++
@@ -1,6 +0,0 @@
-package br.com.cod3r.campoMinado.modelo;
-
-public enum CampoEvento {
-	
-	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR;
-}
--- Programação Java/CampoMinadoSwing/src/br/com/cod3r/campoMinado/modelo/CampoObserver.java
+++
@@ -1,7 +0,0 @@
-package br.com.cod3r.campoMinado.modelo;
-
-@FunctionalInterface
-public interface CampoObserver {
-
-	public void eventoOcorreu(Campo campo, CampoEvento evento);
-}
